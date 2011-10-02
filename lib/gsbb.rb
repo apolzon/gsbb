2a6ba19ab878de4eea8ac7bb682e6f378f0c0c1e
Chris Apolzon <apolzon@gmail.com>
2011-10-02 15:11:15 -0700

    added config task (does nothing) and a bunch of empty tests

--- lib/gsbb.rb
+++ lib/gsbb.rb
@@ -19,8 +19,27 @@ class Gsbb < Thor
     end
   end
 
+  desc "config", "Configure overrides to default behavior"
+  def config
+    # how do we want to do this?
+    # gsbb config --cutoff="15 days"
+    # gsbb config --exclude="hackfest"
+    # gsbb config --only-non-merged
+
+    # ==================================
+
+    # gsbb config
+    # What do you want to change?
+    # 1. Cutoff Date
+    # 2. Exclusion Rule
+    # 3. Non-merged branch inclusion
+
+    # Enter new value (#{current_value})
+  end
+
   desc "show", "List stale branches"
   def show
+    # git branch --merged shows branches fully merged into master
     output = "Stale Branches:\n"
     stale_branches.each do |branch|
       output << "#{branch.name} - #{branch.commit.author}, #{(Date.today - branch.commit.authored_date.to_date).to_i} days old - #{branch.commit.authored_date.to_date}"
--- test/gsbb_test.rb
+++ test/gsbb_test.rb
@@ -9,23 +9,37 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'gsbb'))
 ENV['test'] = 'true'
 
 describe Gsbb do
-  it "includes author and branch name in output" do
-    commit = FakeGrit::Commit.new
-    commit.authored_date = Date.today - 22
-    commit.author = "Bobby Bobberson"
+  describe "#config" do
+    it "allows setting the cut-off date"
+    it "allows setting the branch exclusion rule"
+    it "allows configuring the output style"
+    it "allows excluding non-merged branches by default"
+  end
+  describe "#show" do
+    it "includes author and branch name in output" do
+      commit = FakeGrit::Commit.new
+      commit.authored_date = Date.today - 22
+      commit.author = "Bobby Bobberson"
 
-    branch = FakeGrit::Branch.new
-    branch.name = "branchname"
-    branch.commit = commit
+      branch = FakeGrit::Branch.new
+      branch.name = "branchname"
+      branch.commit = commit
 
-    repo = FakeGrit::Repo.new
-    repo.remotes = [branch]
+      repo = FakeGrit::Repo.new
+      repo.remotes = [branch]
 
-    Grit::Repo.expects(:new).returns(repo)
+      Grit::Repo.expects(:new).returns(repo)
 
-    output = Gsbb.new.show
+      output = Gsbb.new.show
 
-    output.must_match /Bobby Bobberson/
-    output.must_match /branchname/
+      output.must_match /Bobby Bobberson/
+      output.must_match /branchname/
+    end
+    it "includes merge-status in output"
+    it "respects pre-configured cut-off date"
+    it "respects pre-configured branch exclusion rule"
+    it "respects pre-configured output style"
+    it "respects pre-configured non-merge branch behavior"
+    it "accepts a flag to exclude non-merged branches"
   end
 end
